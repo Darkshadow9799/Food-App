31bccf2ce55280d77aede5d0bb837a1c6a012830
Darkshadow9799 <jainaayush99.aj@gmail.com>
2020-09-01 12:23:57 +0530

    Version 1

---
+++ src/screens/SearchSceen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, StyleSheet, View } from 'react-native';
+
+const SearchScreen = () => {
+    return (
+        <View>
+            <Text>Search Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
